c8fdfc82470e63555f0ba678511daba9cb18b93c
labib <lchowdhury@bluecatnetworks.com>
2020-04-03 23:22:59 -0400

    reactified modals for portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Portfolio from './components/Portfolio';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Footer from './components/Footer';
-import { render } from '@testing-library/react';
+import PortfolioModal from './components/PortfolioModal';
+
 class App extends React.Component {
   render(){
     const portfolioItems = [
@@ -108,184 +108,14 @@ class App extends React.Component {
               <a className="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i className="fa fa-chevron-up"></i></a>
           </div>
           {/* <!-- Portfolio Modals--><!-- Portfolio Modal 1--> */}
-          <div className="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-labelledby="portfolioModal1Label" aria-hidden="true">
-              <div className="modal-dialog modal-xl" role="document">
-                  <div className="modal-content">
-                      <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i className="fas fa-times"></i></span>
-                      </button>
-                      <div className="modal-body text-center">
-                          <div className="container">
-                              <div className="row justify-content-center">
-                                  <div className="col-lg-8">
-                                      {/* <!-- Portfolio Modal - Title--> */}
-                                      <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
-                                      {/* <!-- Icon Divider--> */}
-                                      <div className="divider-custom">
-                                          <div className="divider-custom-line"></div>
-                                          <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                          <div className="divider-custom-line"></div>
-                                      </div>
-                                      <img className="img-fluid rounded mb-5" src={require("./assets/img/portfolio/cabin.png")} alt="" />
-                                      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                      <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          {/* <!-- Portfolio Modal 2--> */}
-          <div className="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-labelledby="portfolioModal2Label" aria-hidden="true">
-              <div className="modal-dialog modal-xl" role="document">
-                  <div className="modal-content">
-                      <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i className="fas fa-times"></i></span>
-                      </button>
-                      <div className="modal-body text-center">
-                          <div className="container">
-                              <div className="row justify-content-center">
-                                  <div className="col-lg-8">
-                                      {/* <!-- Portfolio Modal - Title--> */}
-                                      <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Tasty Cake</h2>
-                                      {/* <!-- Icon Divider--> */}
-                                      <div className="divider-custom">
-                                          <div className="divider-custom-line"></div>
-                                          <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                          <div className="divider-custom-line"></div>
-                                      </div>
-                                      <img className="img-fluid rounded mb-5" src={require("./assets/img/portfolio/cake.png")} alt="" />
-                                      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                      <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          {/* <!-- Portfolio Modal 3--> */}
-          <div className="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-labelledby="portfolioModal3Label" aria-hidden="true">
-              <div className="modal-dialog modal-xl" role="document">
-                  <div className="modal-content">
-                      <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i className="fas fa-times"></i></span>
-                      </button>
-                      <div className="modal-body text-center">
-                          <div className="container">
-                              <div className="row justify-content-center">
-                                  <div className="col-lg-8">
-                                      {/* <!-- Portfolio Modal - Title--> */}
-                                      <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Circus Tent</h2>
-                                      {/* <!-- Icon Divider--> */}
-                                      <div className="divider-custom">
-                                          <div className="divider-custom-line"></div>
-                                          <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                          <div className="divider-custom-line"></div>
-                                      </div>
-                                      <img className="img-fluid rounded mb-5" src={require("./assets/img/portfolio/circus.png")} alt="" />
-                                      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                      <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          {/* <!-- Portfolio Modal 4--> */}
-          <div className="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-labelledby="portfolioModal4Label" aria-hidden="true">
-              <div className="modal-dialog modal-xl" role="document">
-                  <div className="modal-content">
-                      <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i className="fas fa-times"></i></span>
-                      </button>
-                      <div className="modal-body text-center">
-                          <div className="container">
-                              <div className="row justify-content-center">
-                                  <div className="col-lg-8">
-                                      {/* <!-- Portfolio Modal - Title--> */}
-                                      <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Controller</h2>
-                                      {/* <!-- Icon Divider--> */}
-                                      <div className="divider-custom">
-                                          <div className="divider-custom-line"></div>
-                                          <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                          <div className="divider-custom-line"></div>
-                                      </div>
-                                      <img className="img-fluid rounded mb-5" src={require("./assets/img/portfolio/game.png")} alt="" />
-                                      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                      <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          {/* <!-- Portfolio Modal 5--> */}
-          <div className="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-labelledby="portfolioModal5Label" aria-hidden="true">
-              <div className="modal-dialog modal-xl" role="document">
-                  <div className="modal-content">
-                      <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i className="fas fa-times"></i></span>
-                      </button>
-                      <div className="modal-body text-center">
-                          <div className="container">
-                              <div className="row justify-content-center">
-                                  <div className="col-lg-8">
-                                      {/* <!-- Portfolio Modal - Title--> */}
-                                      <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Locked Safe</h2>
-                                      {/* <!-- Icon Divider--> */}
-                                      <div className="divider-custom">
-                                          <div className="divider-custom-line"></div>
-                                          <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                          <div className="divider-custom-line"></div>
-                                      </div>
-                                      <img className="img-fluid rounded mb-5" src={require("./assets/img/portfolio/safe.png")} alt="" />
-                                      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                      <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          {/* <!-- Portfolio Modal 6--> */}
-          <div className="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-labelledby="portfolioModal6Label" aria-hidden="true">
-              <div className="modal-dialog modal-xl" role="document">
-                  <div className="modal-content">
-                      <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i className="fas fa-times"></i></span>
-                      </button>
-                      <div className="modal-body text-center">
-                          <div className="container">
-                              <div className="row justify-content-center">
-                                  <div className="col-lg-8">
-                                      {/* <!-- Portfolio Modal - Title--> */}
-                                      <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Submarine</h2>
-                                      {/* <!-- Icon Divider--> */}
-                                      <div className="divider-custom">
-                                          <div className="divider-custom-line"></div>
-                                          <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                          <div className="divider-custom-line"></div>
-                                      </div>
-                                      <img className="img-fluid rounded mb-5" src={require("./assets/img/portfolio/submarine.png")} alt="" />
-                                      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                      <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+          {
+            portfolioItems && portfolioItems.map((item, index) =>
+              <PortfolioModal portfolioItem={ item }></PortfolioModal>
+            )
+          }
       </div>
     );
   }
-  
-
 }
 
 export default App;
---
+++ portfolio/src/components/PortfolioModal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default( {portfolioItem} ) => {
+    return (
+        <div className="portfolio-modal modal fade" id={ portfolioItem.modalLabel } tabindex="-1" role="dialog" aria-labelledby={`${portfolioItem.modalLabel}`+"Label"} aria-hidden="true">
+            <div className="modal-dialog modal-xl" role="document">
+                <div className="modal-content">
+                    <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i className="fas fa-times"></i></span>
+                    </button>
+                    <div className="modal-body text-center">
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-8">
+                                    {/* <!-- Portfolio Modal - Title--> */}
+                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">{portfolioItem.modalLabel}</h2>
+                                    {/* <!-- Icon Divider--> */}
+                                    <div className="divider-custom">
+                                        <div className="divider-custom-line"></div>
+                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                        <div className="divider-custom-line"></div>
+                                    </div>
+                                    <img className="img-fluid rounded mb-5" src={portfolioItem.imgSrc} alt="" />
+                                    <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <button className="btn btn-primary" href="#" data-dismiss="modal"><i className="fas fa-times fa-fw"></i>Close Window</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

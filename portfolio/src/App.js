13015bf80260704fe8189f15d3fcd2a753728b02
labib <labibc01@gmail.com>
2021-05-21 18:35:09 -0400

    updated portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,21 +7,41 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import PortfolioModal from './components/PortfolioModal';
 import ContactMe from './components/ContactMe';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner';
 
 class App extends React.Component {
   render(){
     const portfolioItems = [
+      { modalLabel: "Custos - Threat Intelligence",
+        imgSrc: require("./assets/img/portfolio/custos-logo.png"),
+        projectLink: "https://custos-client.herokuapp.com/login",
+        github: "https://github.com/labib-c/Custos-Public",
+      },
       { modalLabel: "CourseHub",
         imgSrc: require("./assets/img/portfolio/coursehub.png"),
-        projectLink: "https://coursehub.ca"
+        projectLink: "https://coursehub.ca",
+        github: "https://github.com/labib-c/coursehub",
+      },
+      { modalLabel: "DeepNet Finder",
+        imgSrc: require("./assets/img/portfolio/neural-net-finder.png"),
+        projectLink: "https://colab.research.google.com/drive/1roQ2yoCT-OseJXWvySHvx7PtqDmgw8An?usp=sharing",
+        github: "https://github.com/labib-c/neuralNet-Finder",
       },
       { modalLabel: "Intro to Machine Learning",
         imgSrc: require("./assets/img/portfolio/machine-learning.png"),
-        projectLink: "https://github.com/labib-c/Intro-to-Machine-Learning"
+        projectLink: "",
+        github: "https://github.com/labib-c/Intro-to-Machine-Learning",
       }, 
       { modalLabel: "Lossless Compression",
         imgSrc: require("./assets/img/portfolio/compression.png"),
-        projectLink: "https://github.com/labib-c/lossless-compression"
+        projectLink: "",
+        github: "https://github.com/labib-c/lossless-compression",
+      },
+      { modalLabel: "EXT2 File System",
+        imgSrc: require("./assets/img/portfolio/file-system.png"),
+        projectLink: "",
+        github: "https://github.com/labib-c/ext2-file-system"
       }
     ]
     const navbarItems = [ "about", "portfolio", "contact"]

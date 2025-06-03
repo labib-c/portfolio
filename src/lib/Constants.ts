f7e1ff2bf7de942f158036acf3d37d585cc27836
labib <labibc01@gmail.com>
2025-06-03 14:02:14 -0400

    Add some content

---
+++ src/lib/Constants.ts
@@ -0,0 +1,5 @@
+export const GithubUrl = 'https://github.com/labib-c';
+export const LinkedInUrl = 'https://www.linkedin.com/in/labib-c/';
+export const ThesisPath = 'static/assets/thesis.pdf';
+export const ResumePath = 'static/assets/resume.pdf'
+export const Email = 'labibc01@gmail.com';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 
 	let { children } = $props();
+	export const ssr = false;
 </script>
 
 {@render children()}

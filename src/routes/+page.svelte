17a746be49f280713d94b06716597f9087697e97
labib <labibc01@gmail.com>
2025-06-06 13:59:47 -0400

    refactor layout and improve theme toggle integration in portfolio page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { ResumePath} from '$lib/Constants';
+    import { ResumePath } from '$lib/Constants';
     import Contact from '$lib/Components/Contact.svelte';
     import Projects from '$lib/Components/Projects.svelte';
     import ThemeToggle from '$lib/Components/ThemeToggle.svelte';
@@ -13,13 +13,16 @@
 </script>
 
 <main class="p-8">
-    <section class="mb-8">
-        <h1 class="text-4xl font-bold mb-4">Labib Chowdhury</h1>
-        <p class="text-lg">Welcome to my personal portfolio!</p>
+    <section class="mb-8 flex items-center justify-between">
+        <h1 class="text-4xl font-bold">Labib Chowdhury</h1>
+        <ThemeToggle />
+    </section>
+    <section class="mb-8 text-center">
+        <p class="text-lg">I am currently a Software Engineer at Goldman Sachs. I hold a Master of Science in Cybersecurity from Queen's University and an Honours Bachelor of Science in Computer Science and Statistics from the University of Toronto. 
+            My interests include infrastructure, machine learning, and cybersecurity. For more details, please see my full resume below!</p>
         <button class="btn btn-primary mt-4">
             <a href={ResumePath} download>Resume</a>
         </button>
-        <ThemeToggle />
     </section>
     <Projects />
     <Contact />
